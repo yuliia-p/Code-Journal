63329bef0a448f07050e45d154f029e6a7ddebdc
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-19 00:48:13 +0000

    Verify that new entries are saved to local storage issue-1-create-an-entry

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 var elImgURL = document.querySelector('.photo');
 var elPhotoUpl = document.querySelector('#photo-url');
+
 elPhotoUpl.addEventListener('input', photoUpload);
 
 function photoUpload(event) {
   elImgURL.setAttribute('src', event.target.value);
+}
+
+var elEntryForm = document.querySelector('#journal-form');
+
+elEntryForm.addEventListener('submit', formSubmit);
+var nextEntryId = 1;
+
+function formSubmit(event) {
+  event.preventDefault();
+  var newObj = {};
+  newObj.title = elEntryForm.elements.title.value;
+  newObj.url = elEntryForm.elements.url.value;
+  newObj.notes = elEntryForm.elements.notes.value;
+  newObj.nextEntryId = nextEntryId++;
+  data.entries.unshift(newObj);
+  elImgURL.setAttribute('src', '/images/placeholder-image-square.jpg');
+  elEntryForm.reset();
 }
