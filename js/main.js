5a7b1abdf3cc4fab665bbe53e5ef10b19da532fd
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-20 06:02:18 +0000

    next step is Ensure that submitting a new journal entry automatically shows the 'entries' view without reloading the page.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,46 @@ function formSubmit(event) {
   elEntryForm.reset();
 
 }
+
+// var clickElements = document.querySelectorAll('.view');
+// // 0: a#entries-button.view
+// // 1: div#entry - form.view
+// // 2: div#entries - view.view
+// // 3: button.view.white - font - style.new - entries - button
+
+// var allEntries = document.querySelector('#entries-button');
+// var newEntryForm = document.querySelector('.new-entries-button');
+// var saveButton = document.querySelector('.save-button');
+
+document.addEventListener('click', click); //
+
+function click(event) {
+  // if (event.target.matches('.view')) {
+  //   for (var i = 0; i < clickElements.length; i++) {
+  //     if (clickElements[i] === event.target) {
+  //       clickElements[i].className = 'view';
+  // //     }
+  // //   }
+  // // }
+
+  var dataViewEntries = document.querySelector('#entries-view'); // entries page
+  var dataViewEntryForm = document.querySelector('#entry-form'); // entry-form page
+  if (event.target.matches('#entries-button')) {
+    dataViewEntryForm.className = 'view' + ' hidden';
+    dataViewEntries.className = 'view';
+  } else if (event.target.matches('.new-entries-button')) {
+    dataViewEntries.className = 'view' + ' hidden';
+    dataViewEntryForm.className = 'view';
+  }
+  // console.log(event.target.matches('#entries-button'));
+  // console.log('event.target', event.target);
+  // // var dataView = event.target.getAttribute('data-view');
+  // console.log('dataViewEntryForm', dataViewEntryForm);
+  // console.log(dataViewEntries);
+  // console.log(clickElements);
+}
+
+// saveButton.addEventListener('submit', formSubmit) {
+//   elEntryForm.appendChild(singleEntry(entry));
+//   elEntryForm.reset();
+// });
